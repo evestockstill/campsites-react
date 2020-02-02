684cf0ef3f9acef8c0403d96838a5af9ef513028
Eve Stockstill <evestockstillpdx@gmail.com>
2020-02-02 01:45:40 -0800

    added data to campinglist

--- src/campingData.js
+++ src/campingData.js
@@ -5,7 +5,8 @@ export const campingData = [
     img: './img/colonial.jpeg',
     place: ' North Cascades National Park',
     campsiteName: ' Colonial Creek Campground',
-    info: 'In addition to the breathtaking scenery, this Washington State campground is a hub for outdoor activities. The popular Thunder Knob Trail starts in the campground. The lakeside location also makes it popular for fishing, kayaking and canoeing, and swimming (but the water is very cold!).'
+    info:
+      'In addition to the breathtaking scenery, this Washington State campground is a hub for outdoor activities. The popular Thunder Knob Trail starts in the campground. The lakeside location also makes it popular for fishing, kayaking and canoeing, and swimming (but the water is very cold!).'
   },
   {
     id: 2,
@@ -13,9 +14,8 @@ export const campingData = [
     img: './img/hoh.jpeg',
     place: 'Olympic National Park',
     campsiteName: 'Hoh Campground',
-    info: 'The Hoh Rain Forest is located in the stretch of the Pacific Northwest rainforest which once spanned the Pacific coast from southeastern Alaska to the central coast of California. The Hoh is one of the finest remaining examples of temperate rainforest in the United States and is one of the park\'s most popular destinations.'
-
-
+    info:
+      'The Hoh Rain Forest is located in the stretch of the Pacific Northwest rainforest which once spanned the Pacific coast from southeastern Alaska to the central coast of California. The Hoh is one of the finest remaining examples of temperate rainforest in the United States and is one of the park\'s most popular destinations.'
   },
   {
     id: 3,
@@ -23,7 +23,8 @@ export const campingData = [
     img: './img/jones.jpeg',
     place: 'Wilson River',
     campsiteName: ' Jones Creek Campground',
-    info: 'Boasting 33 campsites, Jones Creek Campground is the largest campground within the Wilson River Recreation Area. Located on the northern side of the Wilson River across from the Tillamook Forestry Center and Smith Homestead Day Use Area, the campground offers relatively spacious sites that are ideal for mid-sized RVs, 14 walk-in sites that are tucked away and offer privacy, and the only established group campsite along the river.'
+    info:
+      'Boasting 33 campsites, Jones Creek Campground is the largest campground within the Wilson River Recreation Area. Located on the northern side of the Wilson River across from the Tillamook Forestry Center and Smith Homestead Day Use Area, the campground offers relatively spacious sites that are ideal for mid-sized RVs, 14 walk-in sites that are tucked away and offer privacy, and the only established group campsite along the river.'
   },
   {
     id: 4,
@@ -31,7 +32,8 @@ export const campingData = [
     img: './img/wenatche.jpeg',
     place: 'Leavenworth',
     campsiteName: 'Lake Wenatchee State Park',
-    info: 'Lake Wenatchee State Park is peacefully nestled in a forest on the shore of a beautiful, glacier-fed, alpine lake on the eastern slopes of the Northern Cascades. The park has a variety of outdoor activities including boating, canoeing, kayaking, fishing, hiking, mountain biking, swimming, windsurfing, horseback riding, and a whole lot more. '
+    info:
+      'Lake Wenatchee State Park is peacefully nestled in a forest on the shore of a beautiful, glacier-fed, alpine lake on the eastern slopes of the Northern Cascades. The park has a variety of outdoor activities including boating, canoeing, kayaking, fishing, hiking, mountain biking, swimming, windsurfing, horseback riding, and a whole lot more. '
   },
   {
     id: 5,
@@ -39,12 +41,13 @@ export const campingData = [
     place: 'McKenzie River',
     img: './img/paradise.jpeg',
     campsiteName: 'Paradise Campground',
-    info: 'Paradise Campground is indeed a paradise for campers. Tucked within a lush, old-growth forest of Douglas fir and western red cedar, the area portrays a sub-tropical vibe along the banks of the McKenzie River, where catch-and-release trout and steel head fishing is a popular activity, along with whitewater rafting and kayaking.'
+    info:
+      'Paradise Campground is indeed a paradise for campers. Tucked within a lush, old-growth forest of Douglas fir and western red cedar, the area portrays a sub-tropical vibe along the banks of the McKenzie River, where catch-and-release trout and steel head fishing is a popular activity, along with whitewater rafting and kayaking.'
   },
   {
     id: 6,
     state: 'Washington',
-    img: './img/rainier.jpeg',
+    img: './src/img/rainier.jpeg',
     place: 'Rainier National Forest',
     campsiteName: 'White River',
     info: 'White River\'s access to the surrounding environment is unbeatable, including the stunning and surrounding White River Valley. The Wonderland Trail wanders right through the campground, and other hiking trails, including the Glacier Basin Trail, beckon for a great day trip.'
@@ -55,7 +58,8 @@ export const campingData = [
     img: './img/trillium.jpeg',
     place: 'Mt Hood National Forest',
     campsiteName: 'Trillium Lake',
-    info: 'Trillium Lake Campground is located just minutes from Government Camp off of Highway 26 in the beautiful Mount Hood National Forest. The campground sits on the east side of the lake with some campsites nestled up near the shore the lake and others more inland lacking in lakeside views.  The campground is very popular and can fill up during peak season so be sure to either make reservations or show up early to grab a site. '
+    info:
+      'Trillium Lake Campground is located just minutes from Government Camp off of Highway 26 in the beautiful Mount Hood National Forest. The campground sits on the east side of the lake with some campsites nestled up near the shore the lake and others more inland lacking in lakeside views.  The campground is very popular and can fill up during peak season so be sure to either make reservations or show up early to grab a site. '
   },
   {
     id: 8,
@@ -63,6 +67,7 @@ export const campingData = [
     img: './img/deschutes.jpeg',
     place: 'Deschutes National Forest',
     campsiteName: 'Little Crater Lake',
-    info: 'This is one of the most popular campgrounds in all the Deschutes Forest. Be sure to arrive early, because this campground tends to fill by Thursday afternoon for most weekends. The two lakes in the Newberry Monument area are two of the most popular places to camp in Oregon. The lakes may be a bit cold for swimming, but are great for fishing and the views across the lakes are spectacular.'
+    info:
+      'This is one of the most popular campgrounds in all the Deschutes Forest. Be sure to arrive early, because this campground tends to fill by Thursday afternoon for most weekends. The two lakes in the Newberry Monument area are two of the most popular places to camp in Oregon. The lakes may be a bit cold for swimming, but are great for fishing and the views across the lakes are spectacular.'
   }
 ];
--- src/components/campingList/campinglist/CampingList.js
+++ src/components/campingList/campinglist/CampingList.js
@@ -1,16 +1,21 @@
 import React, { Component } from 'react';
 import Camping from '../../camping/Camping';
 import styles from './CampingList.css';
+import { campingData } from '../../../campingData';
 import PropTypes from 'prop-types';
 
 class CampingList extends Component {
+  state = {
+    camps: campingData
+  };
   render() {
+    const { camps } = this.state;
     return (
-      <div>
-        <section className={styles.campinglist}>
-          <Camping />
-        </section>
-      </div>
+      <section className={styles.campinglist}>
+        {camps.map(camp => {
+          return <Camping key={camp.id} camp={camp} />;
+        })}
+      </section>
     );
   }
 }
